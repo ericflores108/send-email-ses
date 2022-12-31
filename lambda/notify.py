8bd51200cb0d278e4854202f7c5b09e9e0ca9237
Eric Flores <95382086+ericflores108@users.noreply.github.com>
2022-12-31 14:14:30 -0800

    send() method of class

--- lambda/notify.py
+++ lambda/notify.py
@@ -1,41 +1,17 @@
 import json
-import boto3
 from botocore.exceptions import ClientError
 import Email as Mailer
-
-AWS_REGION = "us-west-2"
-
-client = boto3.client('ses', region_name=AWS_REGION)
 
 
 def send_email(recipient, subject, body):
     mailer = Mailer(recipient, subject, body)
 
     try:
-        response = client.send_email(
-            Destination={
-                'ToAddresses': [
-                    mailer.recipient,
-                ],
-            },
-            Message={
-                'Body': {
-                    'Html': {
-                        'Charset': mailer._charset,
-                        'Data': mailer.body_html,
-                    },
-                    'Text': {
-                        'Charset': mailer._charset,
-                        'Data': mailer.body_text,
-                    },
-                },
-                'Subject': {
-                    'Charset': mailer._charset,
-                    'Data': mailer.subject,
-                },
-            },
-            Source=mailer._sender,
-        )
+        print("Sending email to {}...".format(mailer.recipient))
+        print("Subject: {}".format(mailer.subject))
+        print("Body HTML: {}".format(mailer.body_html))
+        print("Body Text: {}".format(mailer.body_text))
+        response = mailer.send()
 
     # Display an error if something goes wrong.
     except ClientError as e:
@@ -47,5 +23,5 @@ def send_email(recipient, subject, body):
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
-    recipient = event['recipient']
-    send_email(recipient)
+    recipient, subject, body = event['recipient'], event['subject'], event['body']
+    send_email(recipient, subject, body)
